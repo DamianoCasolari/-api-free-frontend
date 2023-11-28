81c60620060633b2686728a64c0cb8c109ac7290
DamianoCasolari <damianocasolari@gmail.com>
2023-11-28 14:19:09 +0100

    add index section

---
+++ src/components/appHeader.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    name: "appHeader"
+}
+</script>
+
+<template>
+    <header class="shadow">
+        <div class=" d-flex justify-content-center align-items-center py-2 h-100" >
+            <img src="../../public/img/shadowLogos.png" alt="logo image" class="h-100 me-2">
+            <div class="d-flex flex-column">
+                <div class="fs-1">DevDynasty</div>
+            </div>
+            
+        </div>
+    </header>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/components/appHome.vue
@@ -0,0 +1,93 @@
+<script>
+
+import axios from "axios";
+
+export default {
+    name: "appHome",
+
+    data() {
+        return {
+            'base_API': 'http://127.0.0.1:3000/',
+            'posts_path': 'posts',
+            'error': null,
+            'posts': null
+        }
+    },
+    components: {
+
+    },
+    methods: {
+        getposts(url) {
+            axios.get(url).then(response => {
+                console.log(response);
+                this.posts = response.data.data;
+                this.loading = false;
+
+            }).catch(error => {
+                console.log(error);
+                this.error = error.message
+            })
+        },
+        dragAndScroll() {
+            const container = document.querySelector('.scrollable-container');
+            let isDragging = false;
+            let startX;
+
+            container.addEventListener('mousedown', (e) => {
+                isDragging = true;
+                startX = e.clientX - container.offsetLeft;
+                container.classList.add('grabbing');
+                console.log("ciao");
+            });
+
+            window.addEventListener('mousemove', (e) => {
+                if (!isDragging) return;
+
+                e.preventDefault();
+
+                const x = e.clientX - container.offsetLeft;
+                const deltaX = x - startX;
+                container.scrollLeft -= deltaX;
+
+                startX = x;
+            });
+
+            window.addEventListener('mouseup', () => {
+                isDragging = false;
+                container.classList.remove('grabbing');
+            });
+        }
+    },
+    mounted() {
+        const url = this.base_API + this.posts_path;
+        this.getposts(url);
+        this.dragAndScroll()
+
+    }
+
+}
+</script>
+<template>
+    <section class="index_section">
+
+        <div id="album_rotator_holder" class="d-flex position-relative scrollable-container p-5 h-100">
+
+            
+            <article class=" single_post col-9 col-md-5 col-xxl-3 rounded-4" v-for=" post, index in  posts " v-show="post.published" >
+                <div
+                    class="album_details fs_card d-flex flex-column h-100 p-3 position-relative z-2 ">
+                        <div class="brand fw-semibold strong_text_shadow"> {{ post.title }}
+                        </div>
+                        <div class="model fw-semibold strong_text_shadow">{{ post.model }} </div>
+                        <div class="year strong_text_shadow" style="white-space: wrap;">
+                            <span>{{ post.content }}</span>
+                        </div>
+                </div>
+            </article>
+        </div>
+
+    </section>
+</template>
+
+
+<style lang="scss" scoped></style>
